8405a1b5c158b8689d4bd4b78c3b379453261d6c
Hbatta07 <himalayabatta@gmail.com>
2026-08-10 17:36:23 +0530

    Create data/faculty.ts

---
+++ data/faculty.ts
@@ -0,0 +1,13 @@
+import { FacultyMember } from '@/types/faculty';
+
+export const FACULTY_DATA: FacultyMember[] = [
+  {
+    id: '1',
+    name: '[DATA REQUIRED]',
+    designation: '[DATA REQUIRED]',
+    qualification: '[DATA REQUIRED]',
+    experience: '[DATA REQUIRED]',
+    specialization: '[DATA REQUIRED]',
+    email: '[DATA REQUIRED]',
+  },
+];
