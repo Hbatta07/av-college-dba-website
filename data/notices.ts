804a8c1001dd6c93fbd7ecb899ddcf343afb91c1
Hbatta07 <himalayabatta@gmail.com>
2026-08-10 17:35:59 +0530

    Create data/notices.ts

---
+++ data/notices.ts
@@ -0,0 +1,12 @@
+import { Notice } from '@/types/notice';
+
+export const NOTICES_DATA: Notice[] = [
+  {
+    id: '1',
+    title: '[DATA REQUIRED]',
+    category: 'GENERAL',
+    date: '2026-08-07',
+    description: '[DATA REQUIRED]',
+    isUrgent: false,
+  },
+];
